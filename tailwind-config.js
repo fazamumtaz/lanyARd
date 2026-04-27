addb1689e4c80dabe9a7aee6b8dbaea3c998105d
fazamumtaz <fazamumtazramadhan@gmail.com>
2026-04-27 13:59:26 +0700

    v1

---
+++ tailwind-config.js
@@ -0,0 +1,11 @@
+// Isinya cuma object config aja ⚙️
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3b82f6",
+        accent: "#f59e0b",
+      },
+    },
+  },
+};
